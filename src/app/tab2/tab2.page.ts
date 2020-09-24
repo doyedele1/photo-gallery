3db1aa0c01264415c579b8ab51b6bfa29193fd80
doyedele1 <demilade.oyedele@gmail.com>
2020-09-24 01:29:16 +0100

    building a photo gallery app

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Photo, PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+
+export class Tab2Page {
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) { }
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo: Photo, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // do nothing
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+}
